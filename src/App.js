69075b6ba4dc68bcd1ad830ae1a054a97d60012a
anant-harryfan <85330429+anant-harryfan@users.noreply.github.com>
2023-01-26 14:42:57 +0530

    react1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+// import About from "./Components/About";
+import React, { useState, useSyncExternalStore } from "react";
+import Alert from "./Components/Alert";
+// import { Route, Routes} from "react-router-dom";
 
 function App() {
+  const [Mode, setMode] = useState("light");
+  const [btnText, setBtnText] = useState("Enable Dark Mode");
+  const [alert, setAlert] = useState(null);
+  const [type, setType] = useState("primary");
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (Mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      setBtnText("Enable Light Mode");
+      showAlert("Successfully Enabled DarkMode", "success");
+      document.title = "TextUtils: Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      setBtnText("Enable Dark Mode");
+      showAlert("Successfully Enabled LightMode", "success");
+      document.title = "TextUtils: Light Mode";
+      setType("primary");
+    }
+  };
+
+  const ThemeC = (bgColor, textColor, btn) => {
+    if (Mode === "dark") {
+      document.body.style.backgroundColor = bgColor;
+      document.body.style.color = textColor;
+      setType(btn);
+    } else {
+      showAlert("1st enable dark mode", "danger");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        {/* props matlab bas information hota hai according to page */}
+        {/* <Navbar/> */}
+        <Navbar
+          title="TextUtils"
+          aboutT="About TextUtils"
+          mode={Mode}
+          toggleMode={toggleMode}
+          text={btnText}
+          theme={ThemeC}
+          typo={type}
+        />
+
+        <Alert alert={alert} />
+        <div className="container">
+        {/* <Routes> */}
+        
+        {/* <Route path="/About" element={<About />} /> */}
+        {/* <Route exate path="/About"> <About /> </Route> */}
+      
+        {/* <Route path="/" element={<TextForm heading="Enter the text to analyze" mode={Mode} typo={type}/>}/> */}
+        <TextForm heading="Enter the text to analyze" mode={Mode} typo={type}/>
+        {/* <Route path="/about" element={<About />}/> */}
+      {/* </Routes> */}
+        </div>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("Uppwercase was clicked");
+    // uppercase me convert kar dega
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    // console.log("Uppwercase was clicked");
+    // uppercase me convert kar dega
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (eve) => {
+    // console.log("On change");
+    // type kar pae isliye ye hai
+    setText(eve.target.value);
+  };
+
+  // to text jo value hamne di hai vo hai or setText hogi jo hamne aage denge. settext text ko replace kar degi
+  const [text, setText] = useState("");
+  // // text = "new text"; // Wrong way to change the state
+  // // setText("new text"); // Correct way to change the state
+
+  return (
+    <>
+      <div className="container my-3 mx-6">
+        <h2 style={{color: props.mode==='dark'?'white':'#042743'}}>{props.heading}</h2>
+        <div className="mb-3 txa">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+            style={{backgroundColor: props.mode==='dark'?'#212529':'white', color: props.mode==='dark'?'white':'#042743'}}
+          ></textarea>
+        </div>
+        <button className={`btn btn-${props.typo}`} onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className={`btn btn-${props.typo} mx-3`} onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+      </div>
+      <div className="container my-4" style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{Math.floor(0.008 * text.split(" ").length)} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
